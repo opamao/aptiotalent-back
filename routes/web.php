7ce474fe5a259714be82ed95ed63c0a7ce0b8508
Kadhel AMAN <kadhelaman@MacBook-Air-de-Kadhel.local>
2024-12-05 11:24:19 +0000

    suite

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Authentification
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+Route::get('register', function () {
+    return view('auth.login');
+});
+Route::get('forgot', function () {
+    return view('auth.login');
+});
+Route::get('email-verif', function () {
+    return view('auth.login');
+});
+Route::get('reset-password', function () {
+    return view('auth.login');
+});
+Route::get('two-verif', function () {
+    return view('auth.login');
+});
+Route::get('change-success', function () {
+    return view('auth.login');
 });
