7c1db3583f34f022855e489470385d58f018b3ed
Kadhel AMAN <kadhelaman@192.168.1.6>
2024-12-07 13:15:13 +0000

    change language

--- resources/views/auth/success.blade.php
+++ resources/views/auth/success.blade.php
@@ -46,30 +46,7 @@
         <div class="container-fuild">
             <div class="w-100 overflow-hidden position-relative flex-wrap d-block vh-100">
                 <div class="row">
-                    <div class="col-lg-5">
-                        <div
-                            class="login-background position-relative d-lg-flex align-items-center justify-content-center d-none flex-wrap vh-100">
-                            <div class="bg-overlay-img">
-                                <img src="{{ URL::asset('') }}assets/img/bg/bg-01.png" class="bg-1" alt="Img">
-                                <img src="{{ URL::asset('') }}assets/img/bg/bg-02.png" class="bg-2" alt="Img">
-                                <img src="{{ URL::asset('') }}assets/img/bg/bg-03.png" class="bg-3" alt="Img">
-                            </div>
-                            <div class="authentication-card w-100">
-                                <div class="authen-overlay-item border w-100">
-                                    <h1 class="text-white display-1">Empowering people <br> through seamless HR <br>
-                                        management.</h1>
-                                    <div class="my-4 mx-auto authen-overlay-img">
-                                        <img src="{{ URL::asset('') }}assets/img/bg/authentication-bg-01.png"
-                                            alt="Img">
-                                    </div>
-                                    <div>
-                                        <p class="text-white fs-20 fw-semibold text-center">Efficiently manage your
-                                            workforce, streamline <br> operations effortlessly.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @include('auth.left')
                     <div class="col-lg-7 col-md-12 col-sm-12">
                         <div class="row justify-content-center align-items-center vh-100 overflow-auto flex-wrap">
                             <div class="col-md-7 mx-auto vh-100">
@@ -83,17 +60,15 @@
                                             <div class="text-center mb-3">
                                                 <img src="{{ URL::asset('') }}assets/img/icons/success-tick.svg"
                                                     alt="icon" class="img-fluid mb-3">
-                                                <h2 class="mb-2">Success</h2>
-                                                <p class="mb-0">Your new password has been successfully saved</p>
+                                                <h2 class="mb-2">Succès</h2>
+                                                <p class="mb-0">Votre mot de passe a été enregistré avec succès</p>
                                             </div>
                                             <div class="mb-3">
-                                                <button type="submit" class="btn btn-primary w-100">Back to Sign
-                                                    In</button>
+                                                <button type="submit" class="btn btn-primary w-100">Retour à la
+                                                    connexion</button>
                                             </div>
                                         </div>
-                                        <div class="mt-5 pb-4 text-center">
-                                            <p class="mb-0 text-gray-9">Copyright &copy; 2024 - Smarthr</p>
-                                        </div>
+                                        @include('auth.footer')
                                     </div>
                                 </form>
                             </div>
